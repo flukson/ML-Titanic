abff868f077bf123d2ba953d9c0fe4ee0e421e0d
Paweł Kowalski <flukson@gmail.com>
2020-08-26 23:48:55 +0200

    Improve train module
    + Add execute function
    + Use train module in main titanic.py file
    + Add generated *pkl model file to ignored files

--- src/train.py
+++ src/train.py
@@ -3,37 +3,37 @@ import pandas as pd
 import pickle as pkl
 import sklearn
 
-# TODO: change to function
+def execute(input_file, model_path):
 
-# Split the data for training.
-df = pd.read_csv("data/train_bf.csv")
+    # Split the data for training.
+    df = pd.read_csv(input_file, sep = ';')
 
-y = df["Survived"]
+    y = df["Survived"]
 
-tr_col = []
-for c in df.columns:
-    if c == "Survived":
-        pass
-    else:
-        tr_col.append(c)
+    tr_col = []
+    for c in df.columns:
+        if c == "Survived":
+            pass
+        else:
+            tr_col.append(c)
 
-# Create a classifier and select scoring methods.
-from sklearn.ensemble import RandomForestClassifier
-clf = RandomForestClassifier(n_estimators=10)
+    # Create a classifier and select scoring methods.
+    from sklearn.ensemble import RandomForestClassifier
+    clf = RandomForestClassifier(n_estimators=10)
 
-# Fit full model and predict on both train and test.
-clf.fit(df[tr_col], y)
-preds = clf.predict(df[tr_col])
-metric_name = "train_accuracy"
-metric_result = sklearn.metrics.accuracy_score(y, preds)
+    # Fit full model and predict on both train and test.
+    clf.fit(df[tr_col], y)
+    preds = clf.predict(df[tr_col])
+    metric_name = "train_accuracy"
+    metric_result = sklearn.metrics.accuracy_score(y, preds)
 
-model_pickle = open("data/model.pkl", 'wb')
-pkl.dump(clf, model_pickle)
-model_pickle.close()
+    model_pickle = open(model_path, 'wb')
+    pkl.dump(clf, model_pickle)
+    model_pickle.close()
 
-# Return metrics and model.
-info = ""
-info = info + metric_name
-info = info + " for the model is "
-info = info + str(metric_result)
-print(info)
+    # Return metrics and model.
+    info = ""
+    info = info + metric_name
+    info = info + " for the model is "
+    info = info + str(metric_result)
+    print(info)
--- titanic.py
+++ titanic.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
 
-from src import preprocess, build_features
+from src import preprocess, build_features, train
 
 # paths:
 data_dir = "./data/"
@@ -15,6 +15,8 @@ train_df2_csv = data_dir + "train_df2.csv"
 val_csv = data_dir + "val.csv"
 val_df_csv = data_dir + "val_df.csv"
 
+model_path = data_dir + "model.pkl"
+
 if __name__ == '__main__':
 
     # 1. preprocess
@@ -24,4 +26,6 @@ if __name__ == '__main__':
     build_features.execute(train_df_csv, train_df2_csv)
 
     # 3. train
+    train.execute(train_df2_csv, model_path)
+
     # 4. predict
