bcd04164c2adda6de85130ebf1b47715b7ec3b9c
Paweł Kowalski <flukson@gmail.com>
2020-08-31 21:14:33 +0200

    Rename 'src' subdirectory to 'modules'

--- titanic.py
+++ titanic.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
 
-from src import preprocess, build_features, train, predict
+from modules import preprocess, build_features, train, predict
 
 # paths:
 data_dir = "./data/"
