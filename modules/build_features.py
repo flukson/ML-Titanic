0b2f705c887a7d23f54e46908e579b924807d17b
Paweł Kowalski <flukson@gmail.com>
2020-08-31 23:54:53 +0200

    Improve feature engineering
    + Remove PassengerID
    + Add categorizing Age and Fare for future model selection (some models
      need all features to be in similar ranges)
    + Add Jupyter notebook for explaining chosen values of ranges

--- modules/build_features.py
+++ modules/build_features.py
@@ -4,11 +4,12 @@ import sklearn
 import preprocess
 from common import processed_suffix
 
-def execute(data_file):
+def execute(data_file, categorize=False):
 
     """Builds features
     Args:
         data_file (str): path to input raw csv data file
+        categorize: set to True if Age and Fare should be categorized
     """
 
     # Read preprocessed data:
@@ -17,6 +18,29 @@ def execute(data_file):
     # Replace sex strings with binary value:
     data["Sex"] = data["Sex"].replace("male", 0)
     data["Sex"] = data["Sex"].replace("female", 1)
+
+    if categorize:
+
+        print "Features Age and Fare categorized."
+
+        # Convert age into categories:
+        data["Age"] = data["Age"].astype(int)
+        data.loc[ data["Age"] <= 19, "Age"] = 0
+        data.loc[(data["Age"] > 19) & (data["Age"] <= 25), "Age"] = 1
+        data.loc[(data["Age"] > 25) & (data["Age"] <= 32), "Age"] = 2
+        data.loc[(data["Age"] > 32) & (data["Age"] <= 42), "Age"] = 3
+        data.loc[(data["Age"] > 42), "Age"] = 4
+
+        # Convert fare into categories:
+        data.loc[ data["Fare"] <= 7.854, "Fare"] = 0
+        data.loc[(data["Fare"] > 7.854) & (data["Fare"] <= 10.5), "Fare"] = 1
+        data.loc[(data["Fare"] > 10.5) & (data["Fare"] <= 22.225), "Fare"] = 2
+        data.loc[(data["Fare"] > 22.225) & (data["Fare"] <= 39.688), "Fare"] = 3
+        data.loc[(data["Fare"] > 39.688), "Fare"] = 4
+
+    else:
+
+        print "Features Age and Fare not categorized."
 
     # Embarked: C = Cherbourg, Q = Queenstown, S = Southampton
     # Replace above labels with numbers from 1 to 3:
